a83d05c2782f60f4b68f327f3bbdd92ffd1e81b8
jiro.kirameki <sosyukatu@gmail.com>
2017-04-18 05:31:35 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+
+  private
+
+  def require_user_logged_in
+    #ログイン状態を確認し、ログインしていれば何もせず、
+    #ログインしていなければログインページへ強制的にリダイレクト
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
